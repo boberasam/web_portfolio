c2ef5c4b9163422dc742e32269a02cc13c5d6c00
boris <boberasam@gmail.com>
2025-06-02 11:41:58 -0400

    added Work and Awards section

--- i_hit_my_son_with_my_car_accidentally.js
+++ i_hit_my_son_with_my_car_accidentally.js
@@ -29,6 +29,11 @@ const quotes = [
     "Yikmcbk blghfhghhghhghhhghhhhh. Carol, I hit my son with my car accidentally.",
     "Linux, mmmmmmmmmm.",
     "And I just WONDER where you could get an OS that runs on even a potato and has the ultimate amount of customization, minimal bloat but takes 6 hours in the command line to install a simple program. It's Arch. I'm talking about Arch.",
+    "I bricked my Windows bootloader trying to download Arch (successfully mind you) and now I can only use Linux.",
+    "CAPTAIN! LOOK! I CAN SEE MY HOUSE FROM HERE!",
+    "Hello, world!",
+    "To be fair, you have to have a very high IQ to understand Rick and Morty. The humour is extremely subtle, and without a solid grasp of theoretical physics most of the jokes will go over a typical viewer's head. There's also Rick's nihilistic outlook, which is deftly woven into his characterisation - his personal philosophy draws heavily from Narodnaya Volya literature, for instance. The fans understand this stuff; they have the intellectual capacity to truly appreciate the depths of these jokes, to realize that they're not just funny- they say something deep about LIFE. As a consequence people who dislike Rick and Morty truly ARE idiots- of course they wouldn't appreciate, for instance, the humour in Rick's existencial catchphrase 'Wubba Lubba Dub Dub,' which itself is a cryptic reference to Turgenev's Russian epic Fathers and Sons I'm smirking right now just imagining one of those addlepated simpletons scratching their heads in confusion as Dan Harmon's genius unfolds itself on their television screens. What fools... how I pity them. 😂 And yes by the way, I DO have a Rick and Morty tattoo. And no, you cannot see it. It's for the ladies' eyes only- And even they have to demonstrate that they're within 5 IQ points of my own (preferably lower) beforehand.",
+    "La, la, la la wait till I get my money right",
 ];
 
 function generate_quote() {
